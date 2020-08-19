2db65f45103715d235712bf1fdfd86f42020316f
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-08-19 09:08:27 -0300

    setup components

---
+++ frontend/src/components/templates/Logo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Logo.css'
+
+export default props => 
+    <asside className="logo">
+        Logo
+    </asside>
---
+++ frontend/src/components/templates/Nav.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Nav.css'
+
+
+export default props =>
+    <nav className="nav">
+        nav
+    </nav>
