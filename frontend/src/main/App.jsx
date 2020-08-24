0829918d36fdf1e201c12139d63a39751fe184c5
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-08-24 19:06:02 -0300

    header

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 import Logo from '../components/templates/Logo'
 import Nav from '../components/templates/Nav'
@@ -10,6 +12,6 @@ export default props =>
     <div className="app">
         <Logo/>
         <Nav />
-        <Main/>
+        <Main icon="home" title="inicio" subtitle="a project" />
         <Footer />
     </div>
