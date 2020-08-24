d8637dee839f76bf8403e909938c203e2820cf2f
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-08-24 19:19:18 -0300

    nav

--- frontend/src/components/templates/Nav.jsx
+++ frontend/src/components/templates/Nav.jsx
@@ -4,5 +4,12 @@ import './Nav.css'
 
 export default props =>
     <nav className="nav">
-        nav
+        <nav className="menu">
+            <a href="/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a>
+                <i className="fa fa-users"></i> Usuários   
+            </a>
+        </nav>
     </nav>
