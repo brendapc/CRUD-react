dfa66eaa5f3ffb9d9af86e7faa8f917505825155
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-08-31 15:04:31 -0300

    user

---
+++ frontend/src/components/users/UserCrud.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Main from '../templates/Main'
+
+const headerProps = {
+    icon: 'users',
+    title: 'Usuários',
+    subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir'
+}
+
+export default class UserCrud extends Component{
+    render(){
+        return(
+            <Main {...headerProps}>
+                cadastro de usuário
+            </Main>
+        )
+    }
+    
+}
