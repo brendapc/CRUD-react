0fc48acfe3f164af5fa53148729870ab43ed2a4e
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-08-31 15:56:28 -0300

    esqueleto formulário

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Main from '../templates/Main'
+import axios from 'axios'
 
 const headerProps = {
     icon: 'users',
@@ -7,7 +8,38 @@ const headerProps = {
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+    user: { name: '', email: ''},
+    list: []
+}
+
 export default class UserCrud extends Component{
+
+    state = {... initialState }
+
+    clear (){
+        this.setState({ user: initialState.user })
+    }
+
+    save (){
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios.method(url,user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({user: initialState.user, list})
+            })
+
+    }
+
+    getUpdatedList(user){
+        const list = this.state.list.filter( u => u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
     render() {
         return(
             <Main {...headerProps}>
